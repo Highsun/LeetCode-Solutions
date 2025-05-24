73247c790a067e86ab3e795508f9496238d0a4cc
Highsun <1339687616@qq.com>
2025-05-24 16:09:40 +0800

    20250519-20250525

---
+++ Cpp/2131.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int get_number(char ch) {
+	return ch - 'a';
+}
+
+int main() {
+	vector<string> words = {"lx","xl","cl","cc","cx","ll","lx","xc","xc","cx","ll","xx","xc","xx","xx","lx","lx","xx","cc","xx"};
+
+	int alpha_table[26][26] = {0};
+	for (int i = 0; i < words.size(); i++) {
+		alpha_table[get_number(words[i][0])][get_number(words[i][1])]++;
+	}
+
+	int max_length = 0;
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < i; j++) {
+			if (alpha_table[i][j] && alpha_table[j][i]) {
+				max_length += min(alpha_table[i][j], alpha_table[j][i]) * 4;
+			}
+		}
+	}
+	int max_odd_number = 0;
+	for (int i = 0; i < 26; i++) {
+		if (alpha_table[i][i] % 2 == 1 && alpha_table[i][i] > max_odd_number) {
+			max_odd_number = alpha_table[i][i];
+		}
+	}
+	bool flag = true;
+	for (int i = 0; i < 26; i++) {
+		if (flag) {
+			if (alpha_table[i][i] == max_odd_number) {
+				max_length += alpha_table[i][i] * 2;
+				flag = false;
+				continue;
+			}
+		}
+		max_length += (alpha_table[i][i] / 2) * 4;
+	}
+
+	cout << max_length;
+	return 0;
+}
---
+++ Cpp/3024.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+int main() {
+	vector<int> nums = {3,4,5};
+
+	string result;
+	if ((nums[0] + nums[1] <= nums[2]) || (nums[0] + nums[2] <= nums[1]) || (nums[1] + nums[2] <= nums[0])) result = "none";
+	else if (nums[0] == nums[1] && nums[0] == nums[2]) result = "equilateral";
+	else if ((nums[0] == nums[1] && nums[0] != nums[2]) || (nums[0] == nums[2] && nums[0] != nums[1]) || (nums[1] == nums[2] && nums[1] != nums[0])) result = "isosceles";
+	else result = "scalene";
+
+	cout << result;
+	return 0;
+}
