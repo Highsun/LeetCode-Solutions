f814fd3a7a453c090b0528ae3b4cface547da053
Highsun <1339687616@qq.com>
2026-01-05 17:14:34 +0800

    20260105

---
+++ Cpp/3289.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> nums = {0,3,2,1,3,2};
+
+	std::vector<int> flag(nums.size() - 2, 0);
+	for (int i = 0; i < nums.size(); i++) {
+		flag[nums[i]]++;
+	}
+	std::vector<int> result;
+	for (int i = 0; i < flag.size(); i++) {
+		if (flag[i] > 1) result.push_back(i);
+	}
+	std::cout << result[0] << " " << result[1] << '\n';
+
+	return 0;
+}
