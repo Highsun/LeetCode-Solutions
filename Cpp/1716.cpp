eaa31ab428ac0bd4991da5ad5c8df673620e4f6e
Highsun <1339687616@qq.com>
2026-01-06 18:01:56 +0800

    20260106

---
+++ Cpp/1716.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int n = 10;
+
+	int sum;
+	int k = n / 7;
+	int s = n % 7;
+	sum = 28 * k + 7 * k * (k-1) / 2 + k * s + s * (s+1) / 2;
+	std::cout << sum << '\n';
+
+	return 0;
+}
